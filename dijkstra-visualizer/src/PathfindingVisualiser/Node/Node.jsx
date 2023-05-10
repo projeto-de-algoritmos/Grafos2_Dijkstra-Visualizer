f7d28763a1cb7c2d7d72a1d6104b1cb817228839
Lucas Pimentel <luk.2001@hotmail.com>
2023-05-09 22:03:11 -0300

    Adiciona grid

---
+++ dijkstra-visualizer/src/PathfindingVisualiser/Node/Node.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "./Node.css";
+export default class Node extends Component{
+    constructor(props){
+        super(props);
+        this.state ={};
+    }
+
+    render(){
+        return <div className="node"></div>
+    }
+}
